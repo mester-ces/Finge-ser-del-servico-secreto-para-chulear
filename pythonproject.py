211aa981356b62d1b3401b3609ebc1de5db71e9d
mester-ces <52388120+mester-ces@users.noreply.github.com>
2020-04-21 21:25:27 +0200

    Add files via upload

---
+++ pythonproject.py
@@ -0,0 +1,15 @@
+import getpass
+#Pantalla de Bienvenida
+print("Bienvenido Al Servicio Secreto introduzca su usuario y contraseña")
+
+#Usuario y contraseña
+nombre = input()
+p = getpass.getpass()
+print(f"Bienvenido Agente, {nombre}")
+
+
+
+
+
+
+
